c0583b29e7ff107b40550c3eee369b81d321d2c2
Michał <150702300+mbatsch@users.noreply.github.com>
2023-12-11 11:50:00 +0100

    Update circfit.m

--- circfit.m
+++ circfit.m
@@ -1,7 +1,17 @@
 function   [xc,yc,R,a] = circfit(x,y)
+   % Two solutions may be used
+   % Analytical  by Izhak Bucher 25/oct /1991: 
    x=x(:); y=y(:);
    a=[x y ones(size(x))]\[-(x.^2+y.^2)];
    xc = -.5*a(1);
    yc = -.5*a(2);
    R  =  sqrt((a(1)^2+a(2)^2)/4-a(3));
+   
+   % or MATLAB least squares:
+   % f = @(a) (x-a(1)).^2 + (y-a(2)).^2 - a(3).^2;
+   % a0 = [mean(x),mean(y),max(x)-mean(x)]; 
+   % circFit = lsqnonlin(f,a0);
+   % R=circFit(3);
+   % xc=circFit(1);
+   % yc=circFit(2);
 end
